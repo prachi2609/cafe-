b839044535b8686f6235eb896c8abcbd2d7d871c
prachi2609 <149871795+prachi2609@users.noreply.github.com>
2025-09-16 12:37:38 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,24 @@
+# loops
+
+# for loop
+
+# for i in range(5,11):
+#     print (i)
+
+# for i in range (5):
+#     print("OK")    
+
+
+
+# while loop
+
+add_=1 
+
+while  add_>=10:
+    # print (add_)
+    
+    # add_+= 2
+    print("even")
+    add_+=1
+
+
